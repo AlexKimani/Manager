61b6949805dd4fdd49b08f26d9026e952cd3e859
AlexKimani <alexk249076@gmail.com>
2014-08-08 10:40:23 +0300

    initial code with set dependencies and plugins

---
+++ src/main/java/org/kimani/manager/Manager.java
@@ -0,0 +1,32 @@
+package org.kimani.manager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Hello world!
+ *
+ */
+public class Manager extends HttpServlet
+{
+	private String message;
+
+	public void init() throws ServletException{
+		message = "Hello Kimani";
+	}
+
+	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html");
+
+		PrintWriter out= response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+}
+
+	public void destroy() {
+
+	}
+}
